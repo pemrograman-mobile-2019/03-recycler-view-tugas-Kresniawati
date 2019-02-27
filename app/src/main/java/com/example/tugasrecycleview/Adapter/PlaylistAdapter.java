4358b08f1d2b9aeface5ab08fe60aa65b7e50065
Kresnia <nianichlany666@gmail.com>
2019-02-27 22:08:58 +0700

    main activity

---
+++ app/src/main/java/com/example/tugasrecycleview/Adapter/PlaylistAdapter.java
@@ -0,0 +1,66 @@
+package com.example.tugasrecycleview.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.tugasrecycleview.R;
+import com.example.tugasrecycleview.models.Playlist;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class PlaylistAdapter extends RecyclerView<PlaylistAdapter.ViewHolder> {
+    private List<Playlist> playlistList;
+
+    public PlaylistAdapter(List<Playlist> playlistList){
+        this.playlistList = playlistList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup  viewGroup, int i){
+        Context context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View filmView = inflater.inflate(R.layout.item_playlist, viewGroup, false);
+
+        return new ViewHolder(filmView) {
+        }
+        @Override
+                public void onBindViewHolder(@NonNull ViewHolder ViewHolder viewHolder;
+        viewHolder, int i){
+                Playlist playlist = playlistList.get(i);
+
+            if (playlist.getImg() != null) {
+                Picasso.get().load(playlist.getImg()).into(viewHolder.imageView);
+            } else {
+                Picasso.get().load(playlist.getImg_local()).into(viewHolder.imageView);
+            }
+
+            viewHolder.textView.setText(playlist.getTitle());
+        }
+        @Override
+        public int getItemCount() {
+            return playlistList.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            ImageView imageView;
+            TextView textView;
+            TextView descView;
+
+            ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                imageView = itemView.findViewById(R.id.item_playlist_img);
+                textView = itemView.findViewById(R.id.item_playlist_title);
+                descView = itemView.findViewById(R.id.item_playlist_title_desc);
+            }
+
+        }
--- app/src/main/java/com/example/tugasrecycleview/MainActivity.java
+++ app/src/main/java/com/example/tugasrecycleview/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.tugasrecycleview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.tugasrecycleview.Adapter.PlaylistAdapter;
+import com.example.tugasrecycleview.models.Playlist;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<Playlist> playlistList = new ArrayList<>();
+        playlistList.add(new Playlist("album_1", "UIN Maulana Malik Ibrahim","",R.drawable.uin));
+
+
+        PlaylistAdapter adapterPlaylist = new PlaylistAdapter(playlistList);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_Playlist);
+        recyclerView.setAdapter(adapterPlaylist;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
